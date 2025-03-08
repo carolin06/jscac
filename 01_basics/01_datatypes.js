69a45d5fd48a73129588ae88f177914a59cf1d79
carolin06 <carolinanto906@gmail.com>
2025-03-08 12:12:00 +0000

    func change 8th

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -11,3 +11,15 @@ console.log(typeof age)
 
 //console.log(typeof NULL) object
 //console.log(typeof undefined) undefined
+
+//value of null is 0 and that of undefined is NaN
+// if we convert a string to number, the value will become NaN
+//value of boolean of empty string is false
+//value of boolean of string is true
+
+
+const myfunc=function(){
+    console.log("hello")
+
+}
+console.log(typeof myfunc);
