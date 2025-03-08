f8ca5ce472a7aae4187aa02f386e634151e2d496
carolin06 <carolinanto906@gmail.com>
2025-03-08 12:10:37 +0000

    new changes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"
+let age=20
+//alert(3+3) 
+//range of number datatype= 2^53
+//bigint, string, boolean
+//null is a standalone value
+//undefined
+ 
+console.log(typeof "car")
+console.log(typeof age)
+
+//console.log(typeof NULL) object
+//console.log(typeof undefined) undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=144553
+let accountEmail="car@gmail.com"
+var accPass="12345"
+accCty="jaipur"
+ let acState;
+//var has issue in block and functional scope
+
+console.table([accountId,accountEmail,accPass,accCty,acState])
